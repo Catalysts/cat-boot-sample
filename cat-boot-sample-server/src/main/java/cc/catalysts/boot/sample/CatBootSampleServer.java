bf0b40832cf3efa9d7991d32db9d3354af1174be
klehner <klaus.lehner@catalysts.cc>
2016-03-15 09:04:56 +0100

    initial commit

---
+++ cat-boot-sample-server/src/main/java/cc/catalysts/boot/sample/CatBootSampleServer.java
@@ -0,0 +1,15 @@
+package cc.catalysts.boot.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Klaus Lehner
+ */
+@SpringBootApplication
+public class CatBootSampleServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatBootSampleServer.class, args);
+    }
+}
